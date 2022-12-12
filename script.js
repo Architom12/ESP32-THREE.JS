5c125cbcad46226c1c66f28fd40e2d8520f4e34f
Architom12 <120349472+Architom12@users.noreply.github.com>
2022-12-12 02:23:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-mpu-6050-web-server/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+let scene, camera, rendered, cube;
+
+function parentWidth(elem) {
+  return elem.parentElement.clientWidth;
+}
+
+function parentHeight(elem) {
+  return elem.parentElement.clientHeight;
+}
+
+function init3D(){
+  container = document.querySelector(".scene");
+
+  //Create scene
+  scene = new THREE.Scene();
+
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 1000;
+
+  //Camera setup
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 5, 30);
+
+  const ambient = new THREE.AmbientLight(0x404040, 2);
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xffffff, 2);
+  light.position.set(50, 50, 100);
+  scene.add(light);
+  //Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  // Create a geometry
+  let loader = new THREE.GLTFLoader();
+  loader.load("./scene.gltf", function(gltf) {
+    
+
+  cube = gltf.scene.children[0];
+  scene.add(cube);
+  camera.position.z = 5;
+  renderer.render(scene, camera);
+}
+
+// Resize the 3D object when the browser window changes size
+function onWindowResize(){
+  camera.aspect = parentWidth(document.getElementById("./scene.gltf")) / parentHeight(document.getElementById("./scene.gltf"));
+  //camera.aspect = window.innerWidth /  window.innerHeight;
+  camera.updateProjectionMatrix();
+  //renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(parentWidth(document.getElementById("./scene.gltf")), parentHeight(document.getElementById("./scene.gltf")));
+
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+// Create the 3D representation
+init3D();
+
+// Create events for the sensor readings
+if (!!window.EventSource) {
+  var source = new EventSource('/events');
+
+  source.addEventListener('open', function(e) {
+    console.log("Events Connected");
+  }, false);
+
+  source.addEventListener('error', function(e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
+
+  source.addEventListener('gyro_readings', function(e) {
+    //console.log("gyro_readings", e.data);
+    var obj = JSON.parse(e.data);
+    document.getElementById("gyroX").innerHTML = obj.gyroX;
+    document.getElementById("gyroY").innerHTML = obj.gyroY;
+    document.getElementById("gyroZ").innerHTML = obj.gyroZ;
+
+    // Change cube rotation after receiving the readinds
+    cube.rotation.x = obj.gyroY;
+    cube.rotation.z = obj.gyroX;
+    cube.rotation.y = obj.gyroZ;
+    renderer.render(scene, camera);
+  }, false);
+
+  source.addEventListener('temperature_reading', function(e) {
+    console.log("temperature_reading", e.data);
+    document.getElementById("temp").innerHTML = e.data;
+  }, false);
+
+  source.addEventListener('accelerometer_readings', function(e) {
+    console.log("accelerometer_readings", e.data);
+    var obj = JSON.parse(e.data);
+    document.getElementById("accX").innerHTML = obj.accX;
+    document.getElementById("accY").innerHTML = obj.accY;
+    document.getElementById("accZ").innerHTML = obj.accZ;
+  }, false);
+}
+
+function resetPosition(element){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/"+element.id, true);
+  console.log(element.id);
+  xhr.send();
+}
